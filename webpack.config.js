d5ec351b72e55bc539b4ae529415373023c0f065
Hiagar11 <hiagar11@gmail.com>
2022-05-25 13:38:08 +0300

    Вебпак с бутстрапом

---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+
+const PATHS = {
+    src: path.join(__dirname, 'src')
+}
+
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') {
+    mode = 'production'
+}
+
+console.log(mode + ' mode');
+
+module.exports = {
+    mode: mode,
+    output: {
+        filename: "[name].[contenthash].js",
+        assetModuleFilename: "assets/[hash][ext][query]",
+        clean: true,
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        }
+    },
+    devServer: { // Добавляем элементы конфигурации
+        port: 3333,
+        hot: true,
+        static: path.join(__dirname, "src"),
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader"
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    (mode === 'development') ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    (mode === 'development') ? 'sass-loader' :  {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        'autoprefixer',
+                                    ],
+                                ]
+                            }
+                        }
+                    },
+                    "sass-loader",
+                ]
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,
+                type:    (mode === 'development') ? 'asset/resource' : 'asset/inline',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource'
+            },
+            {
+                test:/\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets:['@babel/preset-env']
+                    }
+                }
+            }
+
+        ]
+
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        }),
+        new htmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ]
+}
