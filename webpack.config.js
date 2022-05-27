38a6d361810bf1da2ff282c46d7d6034b21e9472
Hiagar11 <hiagar11@gmail.com>
2022-05-27 13:20:41 +0300

    Добавил миксины и оптимизаторы изо

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,16 @@ const htmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const webpack = require('webpack');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const {extendDefaultPlugins} = require("svgo");
+const Spritesmith = require('spritesmith');
 
+const sprites = [];
+Spritesmith.run({src: sprites}, function handleResult (err, result) {
+    result.image; // Buffer representation of image
+    result.coordinates; // Object mapping filename to {x, y, width, height} of image
+    result.properties; // Object with metadata about spritesheet {width, height}
+});
 
 const PATHS = {
     src: path.join(__dirname, 'src')
@@ -22,18 +31,6 @@ module.exports = {
         assetModuleFilename: "assets/[hash][ext][query]",
         clean: true,
     },
-    optimization: {
-        splitChunks: {
-            cacheGroups: {
-                styles: {
-                    name: 'styles',
-                    test: /\.css$/,
-                    chunks: 'all',
-                    enforce: true
-                }
-            }
-        }
-    },
     devServer: { // Добавляем элементы конфигурации
         port: 3333,
         hot: true,
@@ -51,7 +48,7 @@ module.exports = {
                 use: [
                     (mode === 'development') ? "style-loader" : MiniCssExtractPlugin.loader,
                     "css-loader",
-                    (mode === 'development') ? 'sass-loader' :  {
+                    (mode === 'development') ? 'sass-loader' : {
                         loader: "postcss-loader",
                         options: {
                             postcssOptions: {
@@ -69,25 +66,74 @@ module.exports = {
             },
             {
                 test: /\.(png|jpg|jpeg|gif|svg)$/i,
-                type:    (mode === 'development') ? 'asset/resource' : 'asset/inline',
+                type: (mode === 'development') ? 'asset/inline' : 'asset/resource',
             },
+
             {
                 test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: 'asset/resource'
             },
             {
-                test:/\.m?js$/,
+                test: /\.m?js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets:['@babel/preset-env']
+                        presets: ['@babel/preset-env']
                     }
                 }
             }
 
-        ]
+        ],
 
+
+    },
+    optimization: {
+        minimizer: [
+            "...",
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminMinify,
+                    options: {
+                        // Lossless optimization with custom option
+                        // Feel free to experiment with options for better result for you
+                        plugins: [
+                            ["gifsicle", {interlaced: true}],
+                            ["jpegtran", {progressive: true}],
+                            ["optipng", {optimizationLevel: 5}],
+                            // Svgo configuration here https://github.com/svg/svgo#configuration
+                            [
+                                "svgo",
+                                {
+                                    plugins: extendDefaultPlugins([
+                                        {
+                                            name: "removeViewBox",
+                                            active: false,
+                                        },
+                                        {
+                                            name: "addAttributesToSVGElement",
+                                            params: {
+                                                attributes: [{xmlns: "http://www.w3.org/2000/svg"}],
+                                            },
+                                        },
+                                    ]),
+                                },
+                            ],
+                        ],
+                    },
+                },
+            }),
+        ],
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true
+                }
+            }
+        }
     },
     plugins: [
         new MiniCssExtractPlugin({
